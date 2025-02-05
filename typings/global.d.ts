b7d0f9006c816201a1c2c03fa922a6441f81ad7e
charles521 <354395702@qq.com>
2025-02-05 10:57:33 +0800

    恢复正常

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -8,3 +8,51 @@ declare module 'process' {
     }
   }
 }
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+// ...
+
+/* IMAGES */
+declare module '*.svg' {
+  const ref: string;
+  export default ref;
+}
+
+declare module '*.bmp' {
+  const ref: string;
+  export default ref;
+}
+
+declare module '*.gif' {
+  const ref: string;
+  export default ref;
+}
+
+declare module '*.jpg' {
+  const ref: string;
+  export default ref;
+}
+
+declare module '*.jpeg' {
+  const ref: string;
+  export default ref;
+}
+
+declare module '*.png' {
+  const ref: string;
+  export default ref;
+}
