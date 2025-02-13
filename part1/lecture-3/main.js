18435100f52aafe94e32a63f46d7052312590a5d
charles521 <354395702@qq.com>
2025-02-13 15:55:49 +0800

    修改目录结构

---
+++ part1/lecture-3/main.js
@@ -0,0 +1,15 @@
+function AppContent() {
+  const [text, setText] = React.useState("");
+
+  return (
+    <main>
+      <input onChange={(event) => setText(event.target.value)} />
+      <p>{text}</p>
+    </main>
+  );
+}
+
+const appEl = document.querySelector("#app");
+const root = ReactDOM.createRoot(appEl);
+
+root.render(<AppContent />);
