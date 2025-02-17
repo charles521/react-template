79735835a66ffeaaa188e8cf3402ebc2ab7492b6
charles521 <354395702@qq.com>
2025-02-17 14:06:02 +0800

    完善动态圆形后续逻辑

--- part1/challenge-1/final/main.js
+++ part1/challenge-1/final/main.js
@@ -1,26 +1,46 @@
 function AppContent() {
+  const [bgClass, setBgClass] = React.useState("");
+  const [textColor, setTextColor] = React.useState("");
+  const [circleSize, setCircleSize] = React.useState(150);
+
   return (
     <main>
       <label>
         Purpel
-        <input type="text" />
+        <input
+          type="checkbox"
+          onChange={(event) => setBgClass(event.target.checked ? "purple" : "")}
+        />
       </label>
       <label>
         text color
-        <select>
-          <option value="" selected>
+        <select onChange={(event) => setTextColor(event.target.value)}>
+          <option value="text-white" selected>
             White
           </option>
           <option value="text-black">Black</option>
-          <option value="text-green">Green</option>
+          <option value="text-orange">Orange</option>
         </select>
       </label>
       <label>
         Circle Size
-        <input type="number" value="150" />
+        <input
+          type="number"
+          value={circleSize}
+          onChange={(event) => setCircleSize(event.target.value)}
+        />
       </label>
 
-      <div className="circle purple">Hi!</div>
+      <div
+        className={`circle ${bgClass} ${textColor}`}
+        style={{
+          width: `${circleSize}px`,
+          height: `${circleSize}px`,
+          lineHeight: `${circleSize}px`,
+        }}
+      >
+        Hi!
+      </div>
     </main>
   );
 }
