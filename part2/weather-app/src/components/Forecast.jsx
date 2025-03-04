050107d9965abea6c944f1dce312a718f4a62f2e
charles521 <354395702@qq.com>
2025-03-04 14:37:48 +0800

    修改

--- part2/weather-app/src/components/Forecast.jsx
+++ part2/weather-app/src/components/Forecast.jsx
@@ -31,7 +31,6 @@ function Forecast({ setIsHome, position }) {
           Forecast
         </Link>
       </Breadcrumbs>
-
       {isLoading && <CircularProgress />}
       {!isLoading && (
         <List
