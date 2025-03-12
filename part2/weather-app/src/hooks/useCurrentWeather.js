5379df4459d30c5dba2de7270425f20da0a4e513
charles521 <354395702@qq.com>
2025-03-12 15:59:35 +0800

    优化hook

--- part2/weather-app/src/hooks/useCurrentWeather.js
+++ part2/weather-app/src/hooks/useCurrentWeather.js
@@ -1,35 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { getWeatherData } from "../services/apiWeather";
-
-// export function useCurrentWeather(position) {
-//   const [temperature, setTemperature] = useState(null);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [weatherIcon, setWeatherIcon] = useState("");
-//   const [cityName, setCityName] = useState("");
-
-//   useEffect(() => {
-//     async function lodeData() {
-//       setIsLoading(true);
-//       const weatherData = await getWeatherData(
-//         position.latitude,
-//         position.longitude
-//       );
-//       console.log(weatherData, "weatherData");
-//       setTemperature({
-//         max: weatherData.main.temp_max,
-//         min: weatherData.main.temp_min,
-//       });
-//       setWeatherIcon(weatherData.weather[0].icon);
-//       setCityName(weatherData.name);
-//       setIsLoading(false);
-//     }
-
-//     lodeData();
-//   }, []);
-
-//   return { cityName, temperature, weatherIcon, isLoading };
-// }
-
 import { useState, useEffect } from "react";
 import { getWeatherData } from "../services/apiWeather";
 
--- part2/weather-app/src/hooks/useForecastWeather.js
+++ part2/weather-app/src/hooks/useForecastWeather.js
@@ -1,28 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { getForecasttWeather } from "../services/apiWeather";
-
-// export function useForecastWeather(position) {
-//   const [temperatureList, setTemperatureList] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   useEffect(() => {
-//     async function lodeData() {
-//       setIsLoading(true);
-//       const forecastWeatherData = await getForecasttWeather(
-//         position.latitude,
-//         position.longitude
-//       );
-
-//       setTemperatureList(forecastWeatherData.list);
-//       setIsLoading(false);
-//     }
-
-//     lodeData();
-//   }, []);
-
-//   return { temperatureList, isLoading };
-// }
-
 import { useEffect, useState } from "react";
 import { getForecasttWeather } from "../services/apiWeather";
 
